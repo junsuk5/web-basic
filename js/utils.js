4dd150ccf3a314f4d4213948ec662b93800c7f66
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-05-09 12:24:20 +0900

    javascript 연습

---
+++ js/utils.js
@@ -0,0 +1,14 @@
+// return X, parameter X
+function hi() {
+  // alert("Hi!!!!!!");
+  console.log("Hi!!!!!!!");
+}
+
+function sum(x, y) {
+  let result = x + y;
+  console.log(result);
+}
+
+function total(x, y) {
+  return x + y;
+}
