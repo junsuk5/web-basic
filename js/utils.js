78738b54a05193ab829286492c74947a24728178
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-05-09 14:17:54 +0900

    js 실습 2

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,13 @@
+function solusion01() {
+  let name = null;
+  while (name === null) {
+    alert("입력해주세요");
+    name = prompt("이름 입력");
+  }
+  console.log(name);
+  alert(name + "님 안녕하세요!");
+}
+
 // return X, parameter X
 function hi() {
   // alert("Hi!!!!!!");
